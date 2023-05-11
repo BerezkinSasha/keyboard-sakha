d2526e49feecee0b79e1f0947b55639295394763
OstiZ <93890262+valeron02@users.noreply.github.com>
2023-05-11 21:49:52 +0900

    second commit

---
+++ app/src/main/java/com/example/keyboard/Constants.java
@@ -0,0 +1,17 @@
+package com.example.keyboard;
+
+public class Constants {
+
+    public static class KeyCode {
+
+        public static final int SPACE = 32;
+
+        public static final int RETURN = 10;
+
+    }
+
+    public enum KEYS_TYPE {
+        SYMBOLS, SAKHA
+    }
+
+}
---
+++ app/src/main/java/com/example/keyboard/SimpleIME.java
@@ -0,0 +1,182 @@
+package com.example.keyboard;
+
+import static android.content.Context.AUDIO_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.annotation.SuppressLint;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.media.AudioManager;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class SimpleIME extends InputMethodService
+        implements KeyboardView.OnKeyboardActionListener {
+
+    public static final String TAG = "KEYBOARD";
+
+    private KeyboardView mKeyboardView;
+    private Keyboard mKeyboard;
+    private Constants.KEYS_TYPE mCurrentLocale;
+    private Constants.KEYS_TYPE mPreviousLocale;
+    private boolean isCapsOn = true;
+
+    @SuppressLint("InflateParams")
+    @Override
+    public View onCreateInputView() {
+        mKeyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
+        mCurrentLocale = Constants.KEYS_TYPE.SAKHA;
+        mKeyboard = getKeyboard(mCurrentLocale);
+        mKeyboard.setShifted(isCapsOn);
+        mKeyboardView.setKeyboard(mKeyboard);
+        mKeyboardView.setOnKeyboardActionListener(this);
+
+        return mKeyboardView;
+    }
+
+    /**
+     * @param locale - keys of keyboard
+     * @return localized keyboard
+     */
+    private Keyboard getKeyboard(Constants.KEYS_TYPE locale) {
+        switch (locale) {
+            case SAKHA:
+                return new Keyboard(this, R.xml.keys_definition_skh);
+            case SYMBOLS:
+                return new Keyboard(this, R.xml.keys_definition_symbols);
+            default:
+                return new Keyboard(this, R.xml.keys_definition_skh);
+        }
+    }
+
+    /**
+     * Play sound on key press
+     *
+     * @param keyCode of pressed key
+     */
+    private void playClick(int keyCode) {
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        switch (keyCode) {
+            case Constants.KeyCode.SPACE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case Keyboard.KEYCODE_DONE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Constants.KeyCode.RETURN:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+            default:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+                break;
+        }
+    }
+
+    @Override
+    public void onPress(int i) {
+        Log.d(TAG, "onPress " + i);
+    }
+
+    @Override
+    public void onRelease(int i) {
+        Log.d(TAG, "onRelease " + i);
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] ints) {
+        Log.d(TAG, "onKey " + primaryCode);
+        InputConnection ic = getCurrentInputConnection();
+        playClick(primaryCode);
+
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE:
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                handleShift();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            case Keyboard.KEYCODE_ALT:
+                handleSymbolsSwitch();
+                break;
+            case Keyboard.KEYCODE_MODE_CHANGE:
+                /*handleLanguageSwitch();*/
+                break;
+            default:
+                char code = (char) primaryCode;
+                if (Character.isLetter(code) && isCapsOn) {
+                    code = Character.toUpperCase(code);
+                }
+
+                ic.commitText(String.valueOf(code), 1);
+                break;
+        }
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+        Log.d(TAG, "onText ");
+    }
+
+    @Override
+    public void swipeLeft() {
+        Log.d(TAG, "swipeLeft ");
+    }
+
+    @Override
+    public void swipeRight() {
+        Log.d(TAG, "swipeRight ");
+    }
+
+    @Override
+    public void swipeDown() {
+        Log.d(TAG, "swipeDown ");
+    }
+
+    @Override
+    public void swipeUp() {
+        Log.d(TAG, "swipeUp ");
+    }
+
+    private void handleSymbolsSwitch() {
+        if (mCurrentLocale != Constants.KEYS_TYPE.SYMBOLS) {
+            mKeyboard = getKeyboard(Constants.KEYS_TYPE.SYMBOLS);
+            mPreviousLocale = mCurrentLocale;
+            mCurrentLocale = Constants.KEYS_TYPE.SYMBOLS;
+        } else {
+            mKeyboard = getKeyboard(mPreviousLocale);
+            mCurrentLocale = mPreviousLocale;
+            mKeyboard.setShifted(isCapsOn);
+        }
+        mKeyboardView.setKeyboard(mKeyboard);
+        mKeyboardView.invalidateAllKeys();
+    }
+
+    private void handleShift() {
+        isCapsOn = !isCapsOn;
+        mKeyboard.setShifted(isCapsOn);
+        mKeyboardView.invalidateAllKeys();
+    }
+
+    /*private void handleLanguageSwitch() {
+        if (mCurrentLocale == Constants.KEYS_TYPE.RUSSIAN) {
+            mCurrentLocale = Constants.KEYS_TYPE.ENGLISH;
+            mKeyboard = getKeyboard(Constants.KEYS_TYPE.ENGLISH);
+        } else {
+            mCurrentLocale = Constants.KEYS_TYPE.RUSSIAN;
+            mKeyboard = getKeyboard(Constants.KEYS_TYPE.RUSSIAN);
+        }
+
+        mKeyboardView.setKeyboard(mKeyboard);
+        mKeyboard.setShifted(isCapsOn);
+        mKeyboardView.invalidateAllKeys();
+    }*/
+}
